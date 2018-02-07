32aa556477a2448245dc7ec518e1cda84e4759e0
victordelval <victor.val.andres@gmail.com>
2018-02-07 14:08:26 +0100

    Defines the propTypes of the dropdown component

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 import './Dropdown.css'
 
+
 /**
- * Multiple search selection dropdown
+ * Multiple search selection dropdown component
  */
 class Dropdown extends React.Component {
+
+    static propTypes = {
+        data: PropTypes.arrayOf(PropTypes.object).isRequired,
+        loading: PropTypes.bool.isRequired
+    }
 
     render() {
         return <select className="dropdown">
