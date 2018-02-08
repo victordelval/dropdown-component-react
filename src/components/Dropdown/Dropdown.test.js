64dddbb976ef3311b318d01fc64f11395c4bc2b3
victordelval <victor.val.andres@gmail.com>
2018-02-08 12:02:01 +0100

    Defines a scenario by grouping the tests of the dropdown component

--- src/components/Dropdown/Dropdown.test.js
+++ src/components/Dropdown/Dropdown.test.js
@@ -4,29 +4,33 @@ import { shallow } from 'enzyme';
 import Dropdown from './Dropdown';
 
 
-it('renders without crashing', () => {
-    shallow(<Dropdown loading={true} data={[]} />);
-});
+describe('Dropdown', () => {
 
-it('renders "loading..." message', () => {
-    const wrapper = shallow(<Dropdown loading={true} data={[]} />);
-    const option = <option>Loading...</option>;
-    // expect(wrapper.contains(welcome)).to.equal(true);
-    expect(wrapper.contains(option)).toEqual(true);
-});
+    it('Should render without crashing', () => {
+        shallow(<Dropdown loading={true} data={[]} />);
+    });
 
-it('renders "no data available" message', () => {
-    const wrapper = shallow(<Dropdown loading={false} data={[]} />);
-    const option = <option>No data available</option>;
-    expect(wrapper.contains(option)).toEqual(true);
-});
+    it('renders "loading..." message', () => {
+        const wrapper = shallow(<Dropdown loading={true} data={[]} />);
+        const option = <option>Loading...</option>;
+        // expect(wrapper.contains(welcome)).to.equal(true);
+        expect(wrapper.contains(option)).toEqual(true);
+    });
 
-it('renders first option', () => {
-    let data = [
-        {"code": "ES", "name": "Spain"},
-        {"code": "PT", "name": "Portugal"}
-    ]
-    const wrapper = shallow(<Dropdown loading={false} data={ data } />);
-    const option = <option value="ES">Spain</option>;
-    expect(wrapper.contains(option)).toEqual(true);
-});
+    it('renders "no data available" message', () => {
+        const wrapper = shallow(<Dropdown loading={false} data={[]} />);
+        const option = <option>No data available</option>;
+        expect(wrapper.contains(option)).toEqual(true);
+    });
+
+    it('renders first option', () => {
+        let data = [
+            {"code": "ES", "name": "Spain"},
+            {"code": "PT", "name": "Portugal"}
+        ]
+        const wrapper = shallow(<Dropdown loading={false} data={ data } />);
+        const option = <option value="ES">Spain</option>;
+        expect(wrapper.contains(option)).toEqual(true);
+    });
+
+})
