357d65f1a972569610f221b7301253433116751c
victordelval <victor.val.andres@gmail.com>
2018-02-11 00:07:41 +0100

    Adds response key wrapper to config file and pass it along with the url from outside the component

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ ReactDOM.render(
             <h1 className="countries-container-text">Dropdown component</h1>
             <hr/>
             <p className="countries-container-text countries-container-p">This is a Multiple Search Selection Dropdown component:</p>
-            <MultiSearchDropdown />
+            <MultiSearchDropdown
+                url={ process.env.REACT_APP_REQUEST_URL }
+                responseKey={ process.env.REACT_APP_RESPONSE_KEY }/>
         </main>
     </Provider>,
     document.getElementById('root')
