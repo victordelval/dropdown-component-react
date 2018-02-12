9718a6688113fef82b4cd4d6a6e4145a41a2bd9c
Victor del Val <victor.val@a3sec.com>
2018-02-12 17:52:30 +0100

    Improves test for MultiSearchDropdown container component including a couple of assertions related to sub components presence

--- src/containers/MultiSearchDropdown/MultiSearchDropdown.test.js
+++ src/containers/MultiSearchDropdown/MultiSearchDropdown.test.js
@@ -4,11 +4,33 @@ import { shallow } from 'enzyme';
 import { connect } from 'react-redux';
 
 import { MultiSearchDropdown } from './MultiSearchDropdown';
+import SelectorBox from '../../components/SelectorBox';
+import SelectorList from '../../components/SelectorList';
 
 
-describe('MultiSearchDropdown', () => {
+describe('<MultiSearchDropdown />', () => {
 
     it('Should render without crashing', () => {
+
+        // mock global fetch
+        const mockResponse = (status, statusText, response) => {
+            return new window.Response(response, {
+                status: status,
+                statusText: statusText,
+                headers: { 'Content-type': 'application/json' }
+            });
+        };
+
+        const mockedData = {
+            countries: [
+                { code: "ES", name: "Spain" },
+                { code: "PT", name: "Portugal" }
+            ]
+        };
+
+        window.fetch = jest.fn().mockImplementation(() =>
+            Promise.resolve(mockResponse(200, null, JSON.stringify(mockedData))));
+
 
         const wrapper = shallow(<MultiSearchDropdown
             dispatch={ () => {} }
@@ -18,6 +40,10 @@ describe('MultiSearchDropdown', () => {
             selected={ [] }
             filtered={ [] }
             url='http://localhost:9000' />);
+
+        expect(wrapper.find('SelectorBox').length).toBe(1);
+        expect(wrapper.find('SelectorList').length).toBe(1);
+        // expect(wrapper.find(<SelectorList />)).to.have.length(1);
     });
 
-})
+});
